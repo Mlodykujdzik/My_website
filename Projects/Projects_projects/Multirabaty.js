c02b7dba90bca72fa1f5b392d0be0761a03d6e33
Dominik <dominikkujda@gmail.com>
2025-03-10 17:42:35 +0100

    added function to calculate the best discount

--- Projects/Projects_projects/Multirabaty.js
+++ Projects/Projects_projects/Multirabaty.js
@@ -16,10 +16,8 @@ function addProduct() {
         alert("Cena po rabacie musi być mniejsza od ceny przed rabatem.");
         return;
     }
-
     products.push({ name, discountedPrice, originalPrice });
     updateProductList();
-
     document.getElementById("product-name").value = "";
     document.getElementById("discounted-price").value = "";
     document.getElementById("original-price").value = "";
@@ -61,6 +59,76 @@ function removeProduct(index) {
     updateProductList();
 }
 function calculateBestDiscount() {
+    sortingByPrice();
+    let normalDiscountSum = products.reduce(
+        (sum, product) => sum + product.discountedPrice,
+        0
+    );
+    let multiDiscountSum = 0;
+    let totalProducts = products.length;
+    let i = 0;
+    while (i < totalProducts) {
+        let groupSize = Math.min(5, totalProducts - i);
+        for (let j = 0; j < groupSize; j++) {
+            let product = products[i + j];
+            if (groupSize === 5 && j === 4) {
+                multiDiscountSum += product.originalPrice * 0.01;
+            } else if (groupSize < 5 && groupSize > 1 && j === groupSize - 1) {
+                if (groupSize === 2) {
+                    multiDiscountSum += product.originalPrice * 0.7;
+                } else if (groupSize === 3) {
+                    multiDiscountSum += product.originalPrice * 0.45;
+                } else if (groupSize === 4) {
+                    multiDiscountSum += product.originalPrice * 0.2;
+                }
+            } else {
+                multiDiscountSum += product.originalPrice;
+            }
+        }
+        i += groupSize;
+    }
+    if (normalDiscountSum < multiDiscountSum) {
+        console.log(
+            "Najlepszy rabat uzyskasz kupując na zwykłym kodzie rabatowym."
+        );
+        console.log(
+            "Suma cen produktów na zwykłym kodzie rabatowym: " +
+                normalDiscountSum.toFixed(2) +
+                " zł"
+        );
+        console.log(
+            "Suma cen produktów na multirabatach: " +
+                multiDiscountSum.toFixed(2) +
+                " zł"
+        );
+    } else if (normalDiscountSum > multiDiscountSum) {
+        console.log("Najlepszy rabat uzyskasz kupując na multirabatach.");
+        console.log(
+            "Suma cen produktów na multirabatach: " +
+                multiDiscountSum.toFixed(2) +
+                " zł"
+        );
+        console.log(
+            "Suma cen produktów na zwykłym kodzie rabatowym: " +
+                normalDiscountSum.toFixed(2) +
+                " zł"
+        );
+    } else {
+        console.log("Oba rabaty są takie same.");
+        console.log(
+            "Suma cen produktów na multirabatach: " +
+                multiDiscountSum.toFixed(2) +
+                " zł"
+        );
+        console.log(
+            "Suma cen produktów na zwykłym kodzie rabatowym: " +
+                normalDiscountSum.toFixed(2) +
+                " zł"
+        );
+    }
+    updateProductList();
+}
+function sortingByPrice() {
     products.sort((a, b) => {
         if (b.originalPrice !== a.originalPrice) {
             return b.originalPrice - a.originalPrice;
@@ -68,5 +136,4 @@ function calculateBestDiscount() {
             return b.discountedPrice - a.discountedPrice;
         }
     });
-    updateProductList();
 }
